4021a594d37b23ee1b0ccb9424654ccfcc265458
vlady <vladysha1021@gmail.com>
2025-04-27 10:13:37 -0400

    Implement server initialization in HttpServer

--- HttpServer.cpp
+++ HttpServer.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
 #include "HttpServer.h"
+
+#pragma comment(lib, "ws2_32.lib")
+
+bool HttpServer::initializeServer() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cout << "WSAStartup failed" << std::endl;
+        return false;
+    }
+
+    serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (serverSocket == INVALID_SOCKET) {
+        std::cout << "Socket creation failed: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return false;
+    }
+
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(port);
+
+    if (bind(serverSocket, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR) {
+        std::cout << "Socket binding failed: " << WSAGetLastError() << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return false;
+    }
+
+    return true;
+}
--- HttpServer.h
+++ HttpServer.h
@@ -1,8 +1,14 @@
-#ifndef HTTPSERVER_H
-#define HTTPSERVER_H
+#pragma once
+
+#include <string>
+#include <winsock2.h>
 
 class HttpServer {
+public:
+    bool initializeServer();
 
+private:
+    SOCKET serverSocket = INVALID_SOCKET;
+    int port = 8080;
 };
 
-#endif //HTTPSERVER_H
